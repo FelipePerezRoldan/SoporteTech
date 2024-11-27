41329727f55253bdd99f1ab7a7d2f6c18728652e
Sala K 401 <salak401@salasfr.itm.edu.co>
2024-11-27 14:14:57 -0500

    Primer commit de SoporteTechApp con Web Forms y Web API

---
+++ SoporteTech.WebAPI.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoporteTech.WebAPI;
+using SoporteTech.WebAPI.Controllers;
+using System.Web.Mvc;
+
+namespace SoporteTech.WebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Disponer
+            HomeController controller = new HomeController();
+
+            // Actuar
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ SoporteTech.WebAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace SoporteTech.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
